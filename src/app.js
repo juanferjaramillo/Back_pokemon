dc90772f4e16b513806e380a59190e1785ba1491
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-04-20 15:33:05 -0500

    cors active

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const server = express();
 server.name = "API";
 
 //==============MIDDLEWARES=========================
-// server.use(cors());
+server.use(cors());
 // server.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
 //      next();
@@ -19,18 +19,18 @@ server.name = "API";
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(express.json());
-//server.use(cookieParser());
+server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 server.use("/", routes);
 server.use((err, req, res, next) => {
   // Error catching endware.
