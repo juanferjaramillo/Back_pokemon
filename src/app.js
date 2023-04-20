fac88c977b94cbe588e7d0ff6eeeb552d04519a3
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-04-20 16:51:55 -0500

    cookie

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ server.name = "API";
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(express.json());
-server.use(cookieParser());
+//server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
